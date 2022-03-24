536e33f05601365533b931f67cc24cc33e57fb17
jlinds2 <71414268+jlinds2@users.noreply.github.com>
2022-03-23 19:21:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,36 +10,7 @@ $('h3').hover(function(){
     $(this).text("User Experience, User Interface and Web Development ");
 });
 
-// $("#slide").mouseover(function(){
-//   $("#panel").show(10);
-//     $("#chicago-sun-times-image").hide(1);       
-// });
-
-// $("#slide").mouseleave(function(){
-//   $('#chicago-sun-times-image').show(1);
-//     $("#panel").hide(1);
-// });
-
 $("#slide, #slide-four").hover(function(){
   $(this).find("#panel, #panel-four").stop().toggle("slow");
 });
 
-// $("#slide-two").mouseover(function(){
-//   $("#panel-two").fadeIn(10);
-//     $("#gig-image").hide(1);       
-// });
-
-// $("#slide-two").mouseleave(function(){
-//   $('#gig-image').fadeIn(1);
-//     $("#panel-two").hide(1);
-// });
-
-// $("#slide-three").mouseover(function(){
-//     $("#panel-three").fadeIn(10);
-//       $("#eaynotes-image").hide(1);       
-//   });
-  
-//   $("#slide-three").mouseleave(function(){
-//     $('#eaynotes-image').fadeIn(1);
-//       $("#panel-three").hide(1);
-//   });

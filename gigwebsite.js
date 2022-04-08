12dc257a09066a21014f3dfa1645994f48e9e9be
jlinds2 <71414268+jlinds2@users.noreply.github.com>
2022-04-08 08:05:24 -0500

    Update gigwebsite.js

--- gigwebsite.js
+++ gigwebsite.js
@@ -1,42 +1,19 @@
-
-//test
-// $(function () {
-//   alert('hello');
-// });
-
-//New to gig
-(function () {
-  $("#slide").slideToggle("slow");
+$("#toggle").click(function() {
+  $("#toggle-navbar").toggle(750);
 });
 
-$("#slide").mouseover(function(){
-    $("#panel").slideToggle("slow");
-  });
-  
-  //Slider
-  // $(document).ready(() => {
-  //   $('#slideshow .slick').slick({
-  //     autoplay: true,
-  //     dots: true,
-  //     slidesToShow: 3,
-  //     responsive: [{
-  //     breakpoint: 750,
-  //     settings: {
-  //     slidesToShow: 1,
-  //     }
-  //   }]
-  //   });
-  // });
+        $(document).ready(() => {
+        $('#slideshow .slick').slick({
+        autoplay: true,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [{
+        breakpoint: 750,
+        settings: {
+        slidesToShow: 1,
+  }
+}]
+});
+});
 
-    $('#slideshow .slick').slick({
-      autoplay: true,
-      dots: true,
-      slidesToShow: 3,
-      responsive: [{
-      breakpoint: 750,
-      settings: {
-      slidesToShow: 1,
-      }
-    }]
-    });
   
